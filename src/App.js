56c899a3c61aecac877aae531701e028c7b5777c
asrbwu <anirban.sarkar@brainwareuniversity.ac.in>
2023-08-11 09:55:47 +0530

    h1 edited

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello artex-gaming</h1>
-
+        <h1>Welcome artex-gaming</h1>
       </header>
     </div>
   );
